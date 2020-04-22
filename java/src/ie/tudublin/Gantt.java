506b04c225b2bea15e5096094c06559e32206c8f
kxngbari <ireadebari@gmail.com>
2020-04-22 18:53:08 +0100

    Adding horizontal words

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,10 +1,14 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
+import java.util.ArrayList;
 
 public class Gantt extends PApplet
-{	
-	
+{
+	ArrayList<Task> tasks = new ArrayList<Task>();
+
 	public void settings()
 	{
 		size(800, 600);
@@ -12,12 +16,20 @@ public class Gantt extends PApplet
 
 	public void loadTasks()
 	{
-		
+		Table table = loadTable("tasks.csv", "header");
+		for(TableRow row:table.rows())
+		{
+			Task task = new Task(row);
+			tasks.add(task);
+		}
 	}
 
 	public void printTasks()
 	{
-		
+        for(Task task:tasks)
+        {
+            println(task);
+        }
 	}
 	
 	public void mousePressed()
@@ -30,14 +42,33 @@ public class Gantt extends PApplet
 		println("Mouse dragged");
 	}
 
-	
-	
-	public void setup() 
-	{
-	}
-	
-	public void draw()
-	{			
-		background(0);
+	public void displayTasks(){
+        float border = width * 0.05f;
+        float x;
+        float y;
+        background(0);
+        colorMode(HSB);
+        stroke(0, 0, 255);
+        textAlign(CENTER, CENTER);
+        for(int i = 1 ; i <= 30 ; i ++)
+        {
+            x = map(i, 0, 30, border, width - border);
+            line(x, border, x, height - border);
+
+            fill(255);
+            text(i, x, border / 2);
+        }
+
+        for(int i=0; i<tasks.size();i++){
+        	Task horiheader = tasks.get(i);
+			y = map(tasks.get(i).start, 0, 30, border, height - border);
+			text(horiheader.getTask(), border / 2, y);
+		}
+    }
+
+	public void setup(){
+		loadTasks();
+		printTasks();
+		displayTasks();
 	}
 }
