fd011176d3604a141aa9149d555cb0e4e11d5501
kxngbari <ireadebari@gmail.com>
2020-04-22 13:59:14 +0100

    adding encapsulation

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,15 +3,16 @@ package ie.tudublin;
 public class Main
 {
 
-    public void gant()
+    public void gantt()
 	{
 		String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Gantt());
     }
-    
+
+
     public static void main(String[] arg)
     {
         Main main = new Main();
-		main.gant();        
+		main.gantt();
     }
 }
